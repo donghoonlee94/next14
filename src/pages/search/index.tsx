5130fcfd75c2562cdbbe408e697138bf1d485531
donghoonlee94 <donghoonlee94@smartscore.kr>
2024-11-11 16:58:42 +0900

    Search layout

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,8 +1,12 @@
 import SearchableLayout from "@/components/searchable-layout";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
 export default function Page() {
-  return <h1>Search</h1>;
+  const router = useRouter();
+  const q = router.query.q;
+
+  return <h1>Search &gt; {q}</h1>;
 }
 
 Page.getLayout = (page: ReactNode) => {
