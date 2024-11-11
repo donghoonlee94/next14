dd00af897aef8209dda3deebede67eb25c981e71
donghoonlee94 <donghoonlee94@smartscore.kr>
2024-11-11 15:50:00 +0900

    Css module

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import style from './index.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h1 className={style.h2}>파랑</h1>
+    </>
+  );
+}
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log("search page");
+  return <h1>Search</h1>;
+}
