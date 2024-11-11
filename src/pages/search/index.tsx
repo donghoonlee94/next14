b5bc1f37e949b91cc20c5f426e065f6c2cde0abc
donghoonlee94 <donghoonlee94@smartscore.kr>
2024-11-11 17:42:48 +0900

    Set styling

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,16 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-  const q = router.query.q;
-
-  return <h1>Search &gt; {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book}></BookItem>
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface BookData {
+  id: number;
+  title: string;
+  subTitle: string;
+  description: string;
+  coverImgUrl: string;
+  publisher: string;
+  author: string;
+}
